10a530cfd308086ea290f97f1033febcc1e5bb98
Samuel Cozannet <samuel.cozannet@madeden.com>
2019-03-17 22:43:57 -0500

    xenial bootstrap

--- juju-ctrllr/00-juju-controller-bootstrap.sh
+++ juju-ctrllr/00-juju-controller-bootstrap.sh
@@ -2,6 +2,6 @@
 #Author: Fatih E. NAR
 set -eaux
 obnum=`hostname | cut -c 10- -`
-time juju bootstrap --config bootstrap-timeout=1200 --to node00vm0ob${obnum}.maas --show-log llc${obnum}-maas  maas-llc${obnum}-controller
+time juju bootstrap --bootstrap-series=xenial --config bootstrap-timeout=1200 --to node00vm0ob${obnum}.maas --show-log llc${obnum}-maas  maas-llc${obnum}-controller
 echo "..."
 juju gui --no-browser --show-credentials
